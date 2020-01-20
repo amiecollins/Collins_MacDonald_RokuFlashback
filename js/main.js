1eb60f46a8a79fd109ed959029e50a6eb20b1bd8
amiecollins <a_collins84448@fanshaweonline.ca>
2020-01-20 18:20:04 -0500

    search fully functional except for parental rating filtering
    search will add up relevance scores based on frequency of query in certain areas of their data such as title, year and keywords

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
-const movies = {
-    lambs: {
+const movies = [
+    {
         id: 1,
         title: "The Silence of the Lambs",
         year: 1991,
@@ -15,7 +15,7 @@ const movies = {
         short_description: "A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.",
         url: "https://storage.cloud.google.com/roku_flashback_media/The%20Silence%20Of%20The%20Lambs%20(1991)/The.Silence.Of.The.Lambs.1991.720p.BluRay.X264.YIFY.mp4"
     },
-    rosemary: {
+    {
         id: 2,
         title: "Rosemarys Baby",
         year: 1968,
@@ -29,9 +29,9 @@ const movies = {
         short_description: "A young couple moves in to an apartment only to be surrounded by peculiar neighbors and occurrences. When the wife becomes mysteriously pregnant, paranoia over the safety of her unborn child begins to control her life.",
         url: "https://storage.cloud.google.com/roku_flashback_media/Rosemarys.Baby.1968.1080p.BluRay.x264.anoXmous/Rosemarys.Baby.1968.1080p.BluRay.x264.anoXmous_.mp4"
     }
-}
-const artists = {
-    coasters: {
+]
+const artists = [
+     {
         id: 1,
         title: "The Coasters",
         year: 1955,
@@ -40,16 +40,19 @@ const artists = {
         local_rating: 9.0,
         genre: "Rhythm and Blues, Rock and Roll",
         keywords: "Leiber and Stoller",
+        short_description: "The Coasters are an American rhythm and blues/rock and roll vocal group who had a string of hits in the late 1950s. Beginning with Searchin and Young Blood, their most memorable songs were written by the songwriting and producing team of Leiber and Stoller. Although the Coasters originated outside of mainstream doo-wop, their records were so frequently imitated that they became an important part of the doo-wop legacy through the 1960s.",
         albums: [
             1
         ]
     }
-}
-const albums = {
-    coasters_collection: {
+]
+const albums = [
+    {
         id: 1,
         title: "The Coasters: Singles",
         year: 1991,
+        artist: "The Coasters",
+        artist_id: 1,
         thumbnail: "https://img.discogs.com/yxxrNE3Uv3AVBqJjaIGYcqHjgd8=/fit-in/500x497/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6349505-1417082410-8653.jpeg.jpg",
         length: "undetermined",
         local_rating: 7.0,
@@ -61,12 +64,14 @@ const albums = {
             1
         ]
     }
-}
-const songs = {
-    yakety: {
+]
+const songs = [
+    {
         id: 1,
         title: "Yakety Yak",
         year: 1991,
+        artist: "The Coasters",
+        artist_id: 1,
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/6/64/Yakety_Yak_by_The_Coasters_US_vinyl_A-side.jpg",
         length: "1min 52sec",
         local_rating: 9.0,
@@ -75,9 +80,9 @@ const songs = {
         keywords: "Cleaning Anthem",
         url: "https://storage.cloud.google.com/roku_flashback_media/The%20Coasters%20The%20Collection%2060%20tracks%20mp3%20cbr%20320/19%20The%20Coasters%20-%20Yakety%20Yak%20stereo.mp3"
     }
-}
-const shows = {
-    show: {
+]
+const shows = [
+    {
         id: 1,
         title: "Placeholder TV Show",
         year: 2020,
@@ -93,11 +98,13 @@ const shows = {
             1
         ]
     }
-}
-const episodes = {
-    episode: {
+]
+const episodes = [
+    {
         id: 1,
         title: "Placeholder Episode",
+        show: "Placeholder TV Show",
+        show_id: 1,
         year: 2020,
         thumbnail: "",
         length: "30min",
@@ -108,7 +115,101 @@ const episodes = {
         short_description: "episode description goes here",
         url: ""
     }
+]
+
+function checkRestrictions(childUser, mediaData) {
+    return mediaData;
 }
+
+function getResult(total, media, user, query) {
+    // check if user is child
+    if (user.isKid) {
+        // remove any content not parent approved
+        media = checkRestrictions(user, media);
+    }
+    // take out all characters that might mess with system, set to lower case (data checked will be set to lowercase as well)
+    query = query.replace(/[^a-zA-Z0-9]/g, "");
+    query = query.toLowerCase();
+    query = query.split(" ");
+    var dataType = ["title", "year", "keywords", "short_description"];
+    // check relevance of each piece of media
+    // check in this order - title, year, keywords, short_description
+    
+    var results = media;
+    
+    for (var v = 0; v < results.length; v++) {
+        results[v].relevance = 0;
+    }
+    for (var dataPhase = 0; dataPhase < 4; dataPhase++) { // repeat while there is still data phases left
+        for (var x = 0; x < results.length; x++) {
+            // get relevance for this results
+            var relevanceScore = checkRelevance(results[x], dataType[dataPhase], query);
+            if (relevanceScore === null){
+                console.log(results[x].title + " is removed")
+                results = results.splice(x);
+            } else {
+                results[x].relevance += relevanceScore;
+            }
+        }
+        // sort by relevance
+        results = sortResults(results, "relevance");
+    }
+    /// idea for later -- spellchecker/omdb suggestions for results that have max relevance < 0.1
+    return results.slice(0, total-1);
+}
+
+function sortResults(unsorted, key) {
+    var sorted = unsorted.sort(function (a,b) {
+        return b[key] - a[key];
+    });
+
+    return sorted;
+}
+
+function getMediaData(media_type, id) {
+    if (media_type === "movie") {
+        return movies.find(x => x.id === id);
+    } else if (media_type === "show") {
+        return shows.find(x => x.id === id);
+    } else if (media_type === "episode") {
+        return episodes.find(x => x.id === id);
+    } else if (media_type === "album") { 
+        return albums.find(x => x.id === id);
+    } else if (media_type === "song") {
+        return songs.find(x => x.id === id);
+    } else if (media_type === "artist") {
+        return artists.find(x => x.id === id);
+    } else {
+        alert("Error");
+        return null;
+    }
+}
+
+function checkRelevance(mediaData, dataType, searchQuery) {
+    var score = 0;
+    if (mediaData.id === undefined || mediaData.id < 0 || mediaData.title === undefined) {
+        return null;
+    } else {
+        for (var y = 0; y < searchQuery.length; y++) {
+        // if checking short_descriptions, make sure it's not a song (if it is, switch data with other relevant info so it's still considered)
+            if (dataType === "short_description" && mediaData.media_type === "song") {
+                var data = mediaData.artist + mediaData.album;
+                data = data.toString().toLowerCase();
+            } else {
+                // check relevance for each term in query
+                console.log("checking " + mediaData.title + ", specifically " + dataType + ". Media type is " + mediaData.media_type);
+                var data = mediaData[dataType].toString().toLowerCase();
+            }
+            if (data.includes(searchQuery[y])) {
+                score++;
+            }
+        }
+        // find average score based on length of query
+        score = score / searchQuery.length;
+        return score;
+    }
+}
+
 const vueApp = (() => {
 
     let vm = new Vue({
@@ -251,18 +352,28 @@ const vueApp = (() => {
                 }
             },
             getSearchResults(query,page,userData) {
+                // check for query
                 if (query !== "" && query !== null) {
-                    if (page === "home") {
-                        var results = { ...movies, ...shows, ...songs, ...artists, ...albums, ...episodes };
-                    } else if (page === "movies") {
-                        var results = { ...movies };
-                    } else if (page === "shows") {
-                        var results = { ...shows, ...episodes };
-                    } else if (page === "music") {
-                        var results = { ...songs, ...artists, ...albums,  };
+                    // check for current page, send available media appropriate for that page
+                    function addMediaType(collection, type) {
+                        for (var a = 0; a < collection.length; a++) {
+                            collection[a].media_type = type;
+                        }
+                        return collection;
                     }
+                    if (page === "home") {
+                        var media = [ ...addMediaType(movies, "movie"), ...addMediaType(shows, "show"), ...addMediaType(songs, "song"), ...addMediaType(artists, "artist"), ...addMediaType(albums, "album"), ...addMediaType(episodes, "episode") ];
+                    } else if (page === "movies") {
+                        var media = [ ...addMediaType(movies, "movie") ];
+                    } else if (page === "shows") {
+                        var media = [ ...addMediaType(shows, "show"), ...addMediaType(episodes, "episode") ];
+                    } else if (page === "music") {
+                        var media = [ ...addMediaType(songs, "song"), ...addMediaType(artists, "artist"), ...addMediaType(albums, "album") ];
+                    }
+                    var results = getResult(5, media, userData, query);
                     return results;
                 } else {
+                    // if query is blank, clear past results
                     return null;
                 }
             },
